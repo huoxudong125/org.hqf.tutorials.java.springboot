e2bef04b7f1e9bfb0bb3c90cefae9102255d919a
huoxudong125 <423huo@163.com>
2021-02-10 01:07:24 +0800

    add webSocket demo

---
+++ web-socket-demo/src/main/java/org/hqf/tutorials/java/springboot/webSocket/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package org.hqf.tutorials.java.springboot.webSocket.controller;
+
+import org.hqf.tutorials.java.springboot.webSocket.dto.Greeting;
+import org.hqf.tutorials.java.springboot.webSocket.dto.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/messages/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
